07d2dff01a5f9667d62f7566202d653eebea3b84
lxdmp <yudmp@sina.com>
2018-11-13 11:06:52 +0800

    some missing in prev commit

--- define.h
+++ define.h
@@ -7,8 +7,8 @@
 
 #include <boost/thread/shared_mutex.hpp>
 typedef boost::shared_mutex rw_mutex_t;
-typedef boost::shared_ptr<rw_mutex_t> read_lock_t;
-typedef boost::shared_ptr<rw_mutex_t> write_lock_t;
+typedef boost::shared_lock<rw_mutex_t> read_lock_t;
+typedef boost::unique_lock<rw_mutex_t> write_lock_t;
 
 #endif
 
